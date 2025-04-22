2928e3f0a78f749e0faabd07bb15ba6da5801abf
ilxplay <illiasuper46@gmail.com>
2025-04-22 19:13:46 +0200

    fixing this before I got a stroke

--- components/isusergoodenough.ts
+++ components/isusergoodenough.ts
@@ -1,24 +1,22 @@
 import { auth } from "@/auth";
 
-export default async function validateuser() {
-	const session = await auth();
-	const github = session?.profile || "no session";
-	if (session !== undefined) {
-		if (github.followers > 3) {
-			return true;
-		} else if (github.public_repos > 2) {
-			return true;
-		} else if (
-			new Date(github.created_at) <
-			new Date(Date.now() - 2 * 30 * 24 * 60 * 60 * 1000)
-		) {
-			return true;
-		} else if (github.public_repos > 1 && github.followers > 1) {
-			return true;
-		} else {
-			return true; // temporarily disable restrictions
-		}
-	} else {
-		return undefined;
-	}
+export default async function validateUser() {
+  const session = await auth();
+  if (!session) {
+    return null;
+  }
+  
+  //in case you want more security later
+  //checks if not null 
+	//better using object then string for given purpose
+	//const github = session.profile || {}; 
+	
+  /*
+  return (
+    github.followers > 3 || github.public_repos > 2 ||
+    (new Date(github.created_at) < new Date(Date.now() - 2 * 30 * 24 * 60 * 60 * 1000)) ||
+    (github.public_repos > 1 && github.followers > 1));
+  */
+  //make it simpler
+  return true;
 }
